f4f63da846b19337d6d692618954bd86dffecd15
indosandi <indosandi@gmail.com>
2017-09-12 19:01:45 +0700

    add examples

---
+++ examples/echo_bot.py
@@ -0,0 +1,11 @@
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+import telebot
+
+API_TOKEN = '406563676:AAFi4ZDmFr1icgfZg7xXJxXH8x3FawSCgbQ'
+
+bot = telebot.TeleBotMediator(API_TOKEN,'sidewinder.rmq.cloudamqp.com','mwauswng','BZDhlLN18X8YvFYriyJFZWUawYbxYK7c',
+                      'mwauswng',5672,True)
+
+bot.polling()
