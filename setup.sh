6b36ba94f3d8f3c5eba5ac429421045a04dcfc26
indosandi <indosandi@gmail.com>
2017-10-25 10:45:39 +0700

    add empty setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+GMAP="XXXXX"
+botToken="XXXXX"
+redisHost='XXXXX'
+redisPort='0000'
+password='XXXXX'
+GSTATICMAP="XXXXX"
+rbtHost='XXXXX'
+rbtPass='XXXXX'
+rbtUser='XXXXX'
+rbtVhost='XXXXX'
+rbtPort='00000'
+
+export botToken
+export GMAP
+export redisHost
+export redisPort
+export password
+export GSTATICMAP
+export rbtHost
+export rbtPass
+export rbtUser
+export rbtVhost
+export rbtPort
